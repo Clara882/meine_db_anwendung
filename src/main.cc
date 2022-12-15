59f8e1b8eefbafd26e1fa1a95276a1d8ed04ff40
clara <clara.stute@stud.thga.de>
2022-12-15 15:53:04 +0100

    Swap eingebaut

--- src/main.cc
+++ src/main.cc
@@ -46,31 +46,41 @@ int main(int argc, char** argv)
         
         }
   */  //std::cout << database_object["Regale"];{"Inhalt"};.size() << std::endl;
-    int a, b;
+    int belegte_plaetze, anzahl_plaetze;
     std::string c, d, e;
-    int leer;
+    int leere_plaetze;
 
     for (auto& element : database_object["Regale"]){
         std::cout << "Regalnummer:" << element["Regal"] << std::endl;
         std::cout << "Anzahl Lagerplätze:" << element["Anzahl Lagerplätze"] << std::endl;
         std::cout << "Inhalt:" << element["Inhalt"] << std::endl;
                 //std::cout << "Leere Plätze:" << element["Leere Plätze"] << std::endl;
-        a = element["Inhalt"].size(); // Anzahl der Elemente in Inhalt
-        b = element["Anzahl Lagerplätze"]; 
-        leer = b - a; // berechnet Leere Plätze
-        std::cout << "Leere Plätze:" << leer << std::endl; // gibt Anzahl der Leeren Plätze aus
+        belegte_plaetze = element["Inhalt"].size(); // Anzahl der Elemente in Inhalt
+        anzahl_plaetze = element["Anzahl Lagerplätze"]; 
+        leere_plaetze = anzahl_plaetze - belegte_plaetze; // berechnet Leere Plätze
+        std::cout << "Leere Plätze:" << leere_plaetze << std::endl; // gibt Anzahl der Leeren Plätze aus
         c = element["Inhalt"].at(2); // setzt c = mit dem 3 Element in Inhalt
         std::cout << c << std::endl; 
-        //e = "abc";
         std::cin >> e; 
-        element["Inhalt"].at(2).swap(e); // tauscht den Inhalt des 3. El
+        element["Inhalt"].at(2).swap(e); // tauscht den Inhalt des 3. Elemente mit e
+        std::cout << "Inhalt:" << element["Inhalt"] << std::endl;
+        element["Inhalt"].at(2).swap(element["Inhalt"].at(1));
+        std::cout << "Inhalt:" << element["Inhalt"] << std::endl;
         c = element["Inhalt"].at(2);
-        //c = element["Inhalt"].at(2);
-        //std::cout << c << std::endl;
-        //std::cout << a << std::endl;
-        //std::cout << leer << std::endl;
         std::cout << "-------------------------------------------------------------------" << std::endl;
-        
+        nlohmann::json::parse(file).update(database_object);
     }
+
+    //nlohmann::json::parse(file).update(database_object);
+
+    
+    /* try
+    {
+        nlohmann::json::parse(file) = database_object;
+    }
+    catch (nlohmann::json::parse_error& ex)
+    {
+        std::cerr << "parse error at byte " << ex.byte << std::endl;
+    }*/
     return 0;
 }
